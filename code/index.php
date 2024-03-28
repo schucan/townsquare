cfb96abf8377ee5a31fd59c3d2d170fedbfcbff9
schucan <schucan@users.noreply.github.com>
2024-03-28 21:32:19 +0100

    Create index.php

---
+++ code/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Real-time Message Board</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="messageArea"></div>
+    <form id="messageForm">
+		<input type="text" id="nameInput" placeholder="Your name..." />
+		<input type="text" id="messageInput" placeholder="Type your message here..." />
+		<button type="submit">Send</button>
+	</form>
+
+    <script src="script.js"></script>
+</body>
+</html>
